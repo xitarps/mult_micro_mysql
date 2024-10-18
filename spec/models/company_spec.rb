ed5608de23623d1631a54c7733de9f6909dfb19b
XitaRps <xitarps@gmail.com>
2024-10-18 01:59:20 -0300

    [Test] Add user, company and leadership_level's models tests

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Company, type: :model do
   end
 
   describe 'validations' do
-    subject { create(:company) }
+    subject { create(:company, :full_structure) }
     it { should validate_presence_of(:name) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,24 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
   end
+
+  context '.handle_managed_users_leaderships' do
+    it 'runs .relocate_managed_users_with_upper_leader' do
+      allow(subject).to receive(:relocate_managed_users_with_upper_leader).and_return('successfully called')
+      result = subject.send(:handle_managed_users_leaderships)
+      expect(result).to eq('successfully called')
+    end
+  end
+
+  context '.relocate_managed_users_with_upper_leader' do
+    it 'runs Users::JoinsLeaderService.call' do
+      params = { user: subject, leader: subject }
+      user_managed_users = [params, params]
+      allow(subject).to receive(:managed_users).and_return(user_managed_users)
+      allow(Users::JoinsLeaderService).to receive(:call).and_return('successfully called')
+
+      result = subject.send(:relocate_managed_users_with_upper_leader)
+      expect(Users::JoinsLeaderService).to have_received(:call).exactly(user_managed_users.size).times
+    end
+  end
 end
